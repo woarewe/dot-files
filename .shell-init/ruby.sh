86a6a109539d888100d4b115d6f1c96f40191084
woarewe <rostislav.zhuravsky@gmail.com>
2023-04-30 22:05:31 +0200

    Automatically switch Ruby versions

--- .shell-init/ruby.sh
+++ .shell-init/ruby.sh
@@ -1,2 +1,18 @@
+autoload -U add-zsh-hook
+
+eval "$(rbenv init - zsh)"
+
 export DISABLE_SPRING=true
 export DISABLE_BOOTSNAP=true
+
+switch-ruby-version() {
+  local version_file=".ruby-version"
+  if [[ -f $version_file ]]; then
+    local ruby_version=$(cat $version_file)
+    rbenv shell $ruby_version
+  else
+    rbenv shell  --unset
+  fi
+}
+
+add-zsh-hook chpwd switch-ruby-version
